a5705c10e68ad2fc50d6668e598609c476ce539d
ferafael <circuncisaofabio@icloud.com>
2022-04-26 12:09:55 -0300

    foram modificados os seguintes: criado as funções de ordenação, criados os check box para poder escolha, mudei os label e criei masi um com o nome.

--- backend/app.py
+++ backend/app.py
@@ -7,30 +7,16 @@ menu_inicia.geometry("500x250+200+200")
 menu_inicia.resizable(True,False)
 menu_inicia.iconbitmap("imagem/icon.ico")
 
-#---------------------dimensão da janela----------------#
-largura = 800
-altura = 400
-#-----------------resolução da janela-----------#
-largura_screen = menu_inicia.winfo_screenwidth()
-altura_screen = menu_inicia.winfo_screenheight()
-
-#-------------------posição da janela-------------------#
-posx = largura_screen/2-largura/2
-posy = altura_screen/2 - altura/2
-
-#---------------------definir a geometry-------------------#
-menu_inicia.geometry("%dx%d+%d+%d" % (largura, altura, posx,posy))
+chckBox = IntVar()
 
 
-#---------------------label-------------------#
-label = Label(menu_inicia, text="Digite o seu nome:").grid(row=0, sticky=W)
+#-----function----#
+def verificar_usuario():
+    l1['text']= text_usuario.get()
+    
+def menu():
+    print(chckBox.get())
 
-#-------------------text---------------------#
-text = Entry(menu_inicia).grid(row=0, column=1)
-
-#-----------------------button------------------#
-btn = Button(menu_inicia, text="Entrar")
-#---------------------Funcões-------------------#
 def bubble_sorted(array):
     n = int(input("Insere os numeros:"))
     if n in array: 
@@ -80,11 +66,79 @@ def select_position(array):
                 min = j
         (array[i],array[min]) = (array[min], array[i])
 
+####fim
 
 
 
+#---------------------dimensão da janela----------------#
+largura = 800
+altura = 500
+#-----------------resolução da janela-----------#
+largura_screen = menu_inicia.winfo_screenwidth()
+altura_screen = menu_inicia.winfo_screenheight()
+
+#-------------------posição da janela-------------------#
+posx = largura_screen/2-largura/2
+posy = altura_screen/2 - altura/2
+
+#---------------------definir a geometry-------------------#
+menu_inicia.geometry("%dx%d+%d+%d" % (largura, altura, posx,posy))
+
+#---------------------Widgets-------------------------------#
+frame_login=Frame(menu_inicia)
+
+label_usuario=Label(frame_login, text="Usuario:")
+text_usuario=Entry(frame_login)
+l1 = Label(frame_login)
+label_qtd=Label(frame_login, text="Quantidade:")
+text_qtd=Entry(frame_login)
+l2 = Label(frame_login)
+btn_acesso=Button(frame_login, text="executar", command=verificar_usuario)
+
+#check box
+check = Checkbutton(
+    menu_inicia,
+    variable=chckBox,
+    text="bubble sorted",
+    command= menu
+)
+check1 = Checkbutton(
+    menu_inicia,
+    variable=chckBox,
+    text="short Bubble sorted",
+    command= menu
+)
+check2 = Checkbutton(
+    menu_inicia,
+    variable=chckBox,
+    text="inserted sorted",
+    command= menu
+)
+check3 = Checkbutton(
+    menu_inicia,
+    variable=chckBox,
+    text="select position",
+    command= menu
+)
+
+
+
+
+
+#---layout--#
+label_usuario.grid(row=1, column=1)
+text_usuario.grid(row=0, column=1)
+btn_acesso.grid(row=3,column=3)
+label_qtd.grid(row=0, column=0)
+text_qtd.grid(row=1, column=1)
+l2.grid(row=1, column=5)
+check.grid(row=5, column=5)
+check1.grid(row=5, column=6)
+check2.grid(row=5, column=7)
+check3.grid(row=5, column=8)
+
+l1.grid()
+frame_login.grid()
 
 #---------packs--------------#
-label.pack()
-btn.pack()
 menu_inicia.mainloop()
